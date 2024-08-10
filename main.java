3110b8103d98500cee11f83f63dd44e00251c738
prakash <prakashdhangi6@gmail.com>
2024-08-10 11:35:35 +0530

    modified

--- main.java
+++ main.java
@@ -5,14 +5,14 @@ public class Main {
         ur.age = 20;
         ur.Name = "prakash";
         ur.Gender = "Male";
-        System.out.println(ur.age + "  " + ur.Name + "  " + ur.Gender);
-        ur.Walk();
+        System.out.println(ur.age + "  " + ur.Name + "  " + ur.Gender);//20 prakash Male
+        ur.Walk(); // Walking
         User ap =new User();
         ap.age=20;
         ap.Name="pooja";
         ap.Gender="female";
-        System.out.println(ap.age+"   "+ap.Name+"  "+ap.Gender);
-        ap.Run();
+        System.out.println(ap.age+"   "+ap.Name+"  "+ap.Gender);//20 pooja female
+        ap.Run();// Running
 
     }
 }
